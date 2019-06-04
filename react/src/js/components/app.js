a5d3e1cbbf5d28ac8b90004ed70a9c05c3593457
Leonidas Esteban <leonidasesteban@gmail.com>
2019-06-04 16:43:47 -0500

    clase 15 - trabajando con react.js

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
 
+console.log(data)
 function App() {
+  const [loaderList, setLoaderList] = useState([])
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
   return (
     <div>
       que linda aplicación hecha en React.js
+      <ul>
+        {
+          loaderList.map(item => <Loader {...item} key={item.id}/> )
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
     </div>
   )
 }
