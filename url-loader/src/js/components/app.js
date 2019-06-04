8b9e89f6ed2937b84cf6826f96126d32f5d8fec8
Leonidas Esteban <leonidasesteban@gmail.com>
2019-06-04 17:09:34 -0500

    clase 16 - Soporte images, fuentes y videos

---
+++ url-loader/src/js/components/app.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
+import logo from '../../images/platzi.png'
+import video from '../../video/que-es-core.mp4'
+
+console.log(data)
+function App() {
+  const [loaderList, setLoaderList] = useState([])
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
+  return (
+    <div>
+      que linda aplicación hecha en React.js
+      <video src={video} width={360} controls poster={logo}></video>
+      <p>
+        <img src={logo} alt="" width={40}/>
+      </p>
+      <ul>
+        {
+          loaderList.map(item => <Loader {...item} key={item.id}/> )
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+    </div>
+  )
+}
+
+export default App
---
+++ url-loader/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname,'src/js/index.js'),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 90000,
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack-dev-server',
+      template: path.resolve(__dirname, 'index.html')
+    }),
+  ]
+}
