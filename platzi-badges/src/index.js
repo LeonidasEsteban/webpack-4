9bbf306ded9d158b083b4b4370d3bef631d2f5da
Leonidas Esteban <leonidasesteban@gmail.com>
2019-06-05 17:05:41 -0500

    clase 21 - setup de platzi badges

---
+++ platzi-badges/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './global.css';
+import App from './components/App';
+
+const container = document.getElementById('app');
+
+ReactDOM.render(<App />, container);
