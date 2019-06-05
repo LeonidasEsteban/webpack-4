263e9db3e0a0c07a65b3b5e9ec4ebad46e3ee94d
Leonidas Esteban <leonidasesteban@gmail.com>
2019-06-04 19:05:52 -0500

    clase 20 - Añadiendo un Dymanic Link Library

---
+++ dll/src/js/contact.js
@@ -0,0 +1,5 @@
+import '../css/estilos.css'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/app'
+render(<App />, document.getElementById('container'))
---
+++ dll/src/js/text.js
@@ -0,0 +1,8 @@
+const text = () => {
+  document.body.innerHTML = '<p>Si ves este texto por 10 segundos pasan cosas</p>'
+}
+
+
+
+
+export default text
